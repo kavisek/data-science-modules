65bc31c85ddb09cf590e077000c622244439e692
kavisek <kavi.skhon@gmail.com>
2018-01-08 15:15:04 -0800

    sql

--- SQL/World Database Practise.sql
+++ SQL/World Database Practise.sql
@@ -32,4 +32,39 @@ SELECT
 		ON l.CountryCode = c.Code
 	GROUP BY c.Code;
 
-#
+# Group BY Summation Query
+SELECT Continent, SUM(Population)
+FROM country
+GROUP BY Continent
+
+# Starting with E and ending in e, wildcard query
+SELECT *
+FROM country
+WHERE Continent LIKE 'E%e'
+
+# Simple Rollup Query on population via continent
+
+
+SELECT Continent, SUM(Population) FROM country
+GROUP BY Continent WITH ROLLUP
+
+# More complex rollup query on population via continent and region
+
+SELECT Continent, Region, SUM(Population) FROM country
+GROUP BY Continent, Region WITH ROLLUP
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
