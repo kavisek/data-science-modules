dabb7b5b6716b91587ef76693b4dcfdce17654fe
Kavi Sekhon <kavi.skhon@gmail.com>
2018-09-13 20:28:28 -0700

    daily practise

--- Other/startup.sh
+++ Other/startup.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
 # Start Jupyter Notebook
-
-cd /Users/kavi/Desktop/
-echo $PATH >> done.txt
-
-cd /Users/kavi/Documents/
-source /Users/kavi/anaconda3/bin/activate main && screen -d -m jupyter notebook
+cd /Users/kavi/Documents/ && source /Users/kavi/anaconda3/bin/activate main && screen -d -m jupyter notebook
