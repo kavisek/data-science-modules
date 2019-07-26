cc37ea18b63a797acb0d573c7f0d74ea79e67926
developer <developer@Kavis-DAD-MacBook-Pro.local>
2019-07-26 10:47:08 -0400

    uploading my argparse scipts

--- Tutorials/Tutorial - Argparse/19-07-25 Argparse Required Flags Script.py
+++ Tutorials/Tutorial - Argparse/19-07-25 Argparse Required Flags Script.py
@@ -12,8 +12,9 @@ if args.limit:
     print(f'limit {args.limit}')
 
 # Command to run the script
-# python 19-07-25\ Argparse\ Required\ Flags\ Script.py
 # python 19-07-25\ Argparse\ Required\ Flags\ Script.py --limit 4
+# python 19-07-25\ Argparse\ Required\ Flags\ Script.py #This fill fail no limit parameter
+
 
 # Notes
 # The 'required=True'  make a flag required or else the script will fail
