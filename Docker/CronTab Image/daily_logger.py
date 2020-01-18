c202e5c959968621c44cec656c95d4e2f4a47d79
Kavi Sekhon <kavi.skhon@gmail.com>
2020-01-18 02:51:05 -0800

    adding logger scripts

---
+++ Docker/CronTab Image/daily_logger.py
@@ -0,0 +1,26 @@
+#!/Users/kavi/anaconda3/envs/nurse/bin/env python3
+
+# Import Modules
+import os
+from datetime import datetime
+
+# Get curernt working directory
+cwd = os.getcwd()
+log_dir = '/logs'
+scirpt_dir = '/logs/daily_logger'
+
+# Create "logs" sub-directory if it doesn't exist
+if not os.path.exists(cwd+log_dir):
+    os.makedirs(cwd+log_dir)
+
+# Create script sub-directory if it doesn't exist
+if not os.path.exists(cwd+scirpt_dir):
+    os.makedirs(cwd+scirpt_dir)
+
+
+# Write down log file with currently Time Stamp
+timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+file_name = timestamp+' log.txt'
+
+with open(cwd+scirpt_dir+'/'+file_name, 'w') as log:
+    log.write(f"This log file was created at {timestamp}")
