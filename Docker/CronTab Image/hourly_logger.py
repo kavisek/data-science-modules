8984110b2d7fa137670b8b96e5828ceb7a4e8e16
Kavi Sekhon <kavi.skhon@gmail.com>
2020-01-18 02:48:40 -0800

    adding shebang ot file

--- Docker/CronTab Image/hourly_logger.py
+++ Docker/CronTab Image/hourly_logger.py
@@ -1,3 +1,6 @@
+#!/Users/kavi/anaconda3/envs/nurse/bin/env python3
+
+# Import Modules
 import os
 from datetime import datetime
 
@@ -14,8 +17,10 @@ if not os.path.exists(cwd+log_dir):
 if not os.path.exists(cwd+scirpt_dir):
     os.makedirs(cwd+scirpt_dir)
 
+
+# Write down log file with currently Time Stamp
 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 file_name = timestamp+' log.txt'
 
 with open(cwd+scirpt_dir+'/'+file_name, 'w') as log:
-    log.write('test')
+    log.write(f"This log file was created at {timestamp}")
