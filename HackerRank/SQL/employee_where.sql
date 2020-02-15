9268cee2a1682a9a5d06afde525a8f3cf3a01314
Kavi Sekhon <kavi.skhon@gmail.com>
2020-02-15 03:12:28 -0500

    complete baisc sql challenges

---
+++ HackerRank/SQL/employee_where.sql
@@ -0,0 +1,6 @@
+-- Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than  per month who have been employees for less than  months. Sort your result by ascending employee_id.
+
+SELECT NAME FROM EMPLOYEE
+WHERE SALARY > 2000
+AND MONTHS < 10
+ORDER BY EMPLOYEE_ID ASC
--- HackerRank/SQL/where_not_string_either_sides.sql
+++ HackerRank/SQL/where_not_string_either_sides.sql
@@ -1,4 +1,4 @@
--- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
 
 SELECT DISTINCT CITY FROM STATION
 WHERE (CITY NOT LIKE '%a'
@@ -7,7 +7,7 @@ AND CITY NOT LIKE '%e'
 AND CITY NOT LIKE '%i'
 AND CITY NOT LIKE '%u')
 
-AND
+OR
 
 (CITY NOT LIKE 'a%'
 AND CITY NOT LIKE 'o%'
